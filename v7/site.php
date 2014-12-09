f2f857ae5a070fdd64608d63e615ea46d90eeb3d
Halvor Lyche Strandvoll <halvors@halvors.org>
2014-12-09 13:10:48 +0100

    Added static authors meta tag.

--- v7/site.php
+++ v7/site.php
@@ -23,7 +23,7 @@ class Site {
 				echo '<title>' . $this->getTitle() . '</title>';
 				echo '<meta name="description" content="' . Settings::description . '">';
 				echo '<meta name="keywords" content="' . Settings::keywords . '">';
-				echo '<meta name="author" content="' . implode(', ', Settings::$authors) . '">'; // TODO: Select all authors in this?
+				echo '<meta name="author" content="halvors and petterroea">';
 				echo '<meta charset="UTF-8">';
 				echo '<link rel="shortcut icon" href="images/favicon.ico">';
 				echo '<link rel="stylesheet" type="text/css" href="style/style.css">';
