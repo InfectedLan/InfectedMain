36f79354780aa780195bb66bf8fa341a2b31e07c
Halvor Lyche Strandvoll <halvors@halvors.org>
2016-09-05 13:19:55 +0200

    More work on php injection.

--- v8/pages/contact.php
+++ v8/pages/contact.php
@@ -1,41 +1,53 @@
-        <center id="wrapper">
+<?php
+/**
+ * This file is part of InfectedMain.
+ *
+ * Copyright (C) 2013-2016 Infected <http://infected.no/>.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
-            <div id="Overview_Post" style="margin-bottom:150px;">
-                <i id="main_emblem" style="padding:10px; padding-bottom:20px; border-bottom:white solid 1px; font-size:128px;" class="fa fa-question Foreground2" aria-hidden="true"></i>
-                <h1 style="color:white;">Noe du lurer på?</h1>
-                <h3 style="color:white;">Ta gjerne kontakt med oss!</h3>
-            </div>
+require_once 'handlers/sectionpagehandler.php';
 
-        </center>
+echo '<center id="wrapper">';
+  echo '<div id="Overview_Post" style="margin-bottom:150px;">';
+    echo '<i id="main_emblem" style="padding:10px; padding-bottom:20px; border-bottom:white solid 1px; font-size:128px;" class="fa fa-question Foreground2" aria-hidden="true"></i>';
+    echo '<h1 style="color:white;">Noe du lurer på?</h1>';
+    echo '<h3 style="color:white;">Ta gjerne kontakt med oss!</h3>';
+  echo '</div>';
+echo '</center>';
+echo '<div id="onsite_information" class="Background2">';
 
+  $sectionPage = SectionPageHandler::getSectionPageByName('how-to-contact');
 
-        <div id="onsite_information" class="Background2">
-            <center class="Banner_Post">                      
-                <center style="margin:0 auto; display:inline-block; padding-top:25px; padding-bottom:25px;">
-                    <i class="fa fa-question-circle-o fa-5x Foreground1" aria-hidden="true"></i>
-                    <h2 style="color:black;">Hvordan tar jeg kontakt?</h2>
-                    <p style="color:black; text-align:center;">Vennligst benytt <strong>epost</strong> hvis henvendelsen <strong>ikke haster</strong></p>
-                </center>
-            </center>
+  if ($sectionPage != null) {
+    echo $sectionPage->getSection();
+  }
 
-            <center class="Background2" style="padding-top:50px; padding-bottom:50px; min-height:100px;">
-
-                <div class="Banner_Info">
-                    <i class="fa fa-phone fa-4x Foreground1" aria-hidden="true"></i>
-                    <h3>Telefon</h3>
-                    <p style="text-align:center;">Du kan gjerne kontakte oss på telefon.</p>
-                    <p class="Background1 Foreground2" style="margin:20px; padding:10px; text-align: center;">99 76 77 45</p>
-
-                </div>
-
-                
-                <div class="Banner_Info">
-                    <i class="fa fa-envelope-o fa-4x Foreground1" aria-hidden="true"></i>
-                    <h3>Epost</h3>
-                    <p style="text-align:center;">Du kan komme i kontakt med oss på epost</p>
-                    <p class="Background1 Foreground2" style="margin:20px; padding:10px; text-align: center;">kontakt@infected.no</p>
-                </div>
-
-
-            </center> 
-        </div>
+  echo '<center class="Background2" style="padding-top:50px; padding-bottom:50px; min-height:100px;">';
+    echo '<div class="Banner_Info">';
+      echo '<i class="fa fa-phone fa-4x Foreground1" aria-hidden="true"></i>';
+      echo '<h3>Telefon</h3>';
+      echo '<p style="text-align:center;">Du kan gjerne kontakte oss på telefon.</p>';
+      echo '<p class="Background1 Foreground2" style="margin:20px; padding:10px; text-align: center;">99 76 77 45</p>';
+    echo '</div>';
+    echo '<div class="Banner_Info">';
+      echo '<i class="fa fa-envelope-o fa-4x Foreground1" aria-hidden="true"></i>';
+      echo '<h3>E-post</h3>';
+      echo '<p style="text-align:center;">Du kan komme i kontakt med oss på e-post</p>';
+      echo '<p class="Background1 Foreground2" style="margin:20px; padding:10px; text-align: center;">kontakt@infected.no</p>';
+    echo '</div>';
+  echo '</center>';
+echo '</div>';
+?>
